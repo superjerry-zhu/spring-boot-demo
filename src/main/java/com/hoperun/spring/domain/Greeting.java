17fe66649088d9559184fd2b55a1e0ae9d2b289f
zhu_yingjie <zhu_yingjie@hoperun.com>
2017-03-29 23:31:43 +0800

    step 5 build restful service

---
+++ src/main/java/com/hoperun/spring/domain/Greeting.java
@@ -0,0 +1,22 @@
+package com.hoperun.spring.domain;
+
+/**
+ * Created by Administrator on 2017/3/29.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
